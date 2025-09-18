ce24f0315b018ca22298dd8cdc502cad8a1d03ec
Palexander10 <pa.ramonm@uea.edu.ec>
2025-09-18 18:48:14 -0500

    Llamamos al primer metodo por defecto del 10 por ciento

--- src/main/java/com/mycompany/calculodecuento/CalculoDecuento.java
+++ src/main/java/com/mycompany/calculodecuento/CalculoDecuento.java
@@ -17,6 +17,10 @@ public class CalculoDecuento {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double monto = 125.00;
+        double descuentoDefecto = calcularDescuento(monto);
+        double montoFinalDefecto = monto - descuentoDefecto;
+        System.out.println("Descuento por defecto (10%): " + descuentoDefecto);
+        System.out.println("Monto final a pagar: " + montoFinalDefecto);
     }
 }
