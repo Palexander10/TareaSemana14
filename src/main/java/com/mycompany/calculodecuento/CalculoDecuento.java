69ee12fe386b91903f97588677701605a8517f46
Palexander10 <pa.ramonm@uea.edu.ec>
2025-09-18 18:59:48 -0500

    Llamamos al segundo metodo descuento propuesto

--- src/main/java/com/mycompany/calculodecuento/CalculoDecuento.java
+++ src/main/java/com/mycompany/calculodecuento/CalculoDecuento.java
@@ -13,14 +13,19 @@ public class CalculoDecuento {
         return montoTotal * (porcentajeDescuento / 100);
     }
     public static double calcularDescuento(double montoTotal) {
-        return calcularDescuento(montoTotal, 10.0);
+        return calcularDescuento(montoTotal, 10.00);
     }
 
     public static void main(String[] args) {
         double monto = 125.00;
-        double descuentoDefecto = calcularDescuento(monto);
-        double montoFinalDefecto = monto - descuentoDefecto;
-        System.out.println("Descuento por defecto (10%): " + descuentoDefecto);
+        double descuentoporDefecto = calcularDescuento(monto);
+        double montoFinalDefecto = monto - descuentoporDefecto;
+        System.out.println("Descuento por defecto (10%): " + descuentoporDefecto);
         System.out.println("Monto final a pagar: " + montoFinalDefecto);
+        
+        double descuentoPropuesto = calcularDescuento(monto, 25.00);
+        double montoFinalPersonalizado = monto - descuentoPropuesto;
+        System.out.println("\nDescuento propuesto (25%): " + descuentoPropuesto);
+        System.out.println("Monto final a pagar: " + montoFinalPersonalizado);
     }
 }
