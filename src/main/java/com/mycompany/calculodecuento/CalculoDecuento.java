9c87be6e5feb47452b6318a4f004423fa82ace8e
Palexander10 <pa.ramonm@uea.edu.ec>
2025-09-18 18:40:32 -0500

    Metodo descuento especifico y por defecto 10 por ciento

--- src/main/java/com/mycompany/calculodecuento/CalculoDecuento.java
+++ src/main/java/com/mycompany/calculodecuento/CalculoDecuento.java
@@ -9,6 +9,12 @@ package com.mycompany.calculodecuento;
  * @author Pablo Alexander
  */
 public class CalculoDecuento {
+    public static double calcularDescuento(double montoTotal, double porcentajeDescuento) {
+        return montoTotal * (porcentajeDescuento / 100);
+    }
+    public static double calcularDescuento(double montoTotal) {
+        return calcularDescuento(montoTotal, 10.0);
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
